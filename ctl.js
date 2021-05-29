09e7d57ebca948384713631da8add3591cbdc770
authfire29 <85037076+authfire29@users.noreply.github.com>
2021-05-29 15:22:57 -0500

    Add files via upload

---
+++ ctl.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const 	auth = firebase.auth(),
+		provider = new firebase.auth.GoogleAuthProvider();
+
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+auth.languageCode = 'it';
+provider.setCustomParameters({ prompt: "select_account" });
+auth.onAuthStateChanged(
+	usuarioAuth => {
+		if (usuarioAuth && usuarioAuth.email) {
+			email.value = usuarioAuth.email;
+			nombre.value = usuarioAuth.displayName;
+			avartar.scr = usuarioAuth.photoURL;
+		} else {
+			auth.signInWithRedirect(provider);
+		}
+	},
+	procesaError
+);
+
+async function terminarSesion() {
+	try	{
+		await auth.signOut();
+	} catch (e) {
+		procesaError(e);
+	}
+}
+
+function procesaError(e) {
+	console.log(e);
+	alert(e.message);
+}
